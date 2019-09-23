fe5160e394115ed70acb7bc1d433808546142b97
SamGomes <sam.s.gomes@sapo.pt>
2019-09-23 11:53:23 +0100

    minor bug fixes on examples

--- Examples/exampleHelloWorld.py
+++ Examples/exampleHelloWorld.py
@@ -7,7 +7,7 @@ agent = Agent("YOLO")
 controlRef = agent.getControlRef()
 
 
-helloBehavior = ComposedBehavior(controlRef, [BlinkBehaviorEaseInOut(controlRef, Color(rgb=(1.0, 1.0, 1.0)), ColorBrightness.LOW, 2, 2.0, Color(rgb=(0.0, 0.0, 1.0)))])
+helloBehavior = ComposedBehavior(controlRef, [BlinkBehaviorEaseInOut(controlRef, Color(rgb=(1.0, 1.0, 1.0)), ColorBrightness.LOW, 2, 2.0, Color(rgb=(1.0, 1.0, 1.0)))])
 goodbyeBehavior = ComposedBehavior(controlRef, [])
 puppeteerBehavior = ComposedBehavior(controlRef,[])
 idleBehavior = ComposedBehavior(controlRef, [])
